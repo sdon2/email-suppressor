1d36ff94e1bee2f96a57f24b3d36c5f836832ea0
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2024-10-11 16:09:35 +0530

    Added navbar

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -5,19 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    
+
     <title>@yield('title') - Email Suppressor</title>
 </head>
 
-<body>
-    <div class="container mx-auto mt-4">
-        @yield('content')
+<body style="background: #d5d5d5">
+    <div class="container p-0 mx-auto mt-4 border border-black border-top-0">
+        @include('partials/navbar')
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-md-12">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
